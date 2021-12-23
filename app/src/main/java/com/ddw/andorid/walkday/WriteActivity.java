439e2e4b33c1626186ee614bf9dd32b41c691676
gyungmean <yogurt2819@gmail.com>
2021-12-24 02:21:23 +0900

    GoogleMap 연결

--- app/src/main/java/com/ddw/andorid/walkday/WriteActivity.java
+++ app/src/main/java/com/ddw/andorid/walkday/WriteActivity.java
@@ -1,11 +1,113 @@
 package com.ddw.andorid.walkday;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class WriteActivity extends Activity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class WriteActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    final static String TAG = "WriteActivity";
+    final static int PERMISSION_REQ_CODE = 100;
+
+    private GoogleMap mGoogleMap;
+    private MarkerOptions markerOptions;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        mapLoad();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        Log.d(TAG, "Map ready");
+
+        if (checkPermission())
+            mGoogleMap.setMyLocationEnabled(true);
+
+        mGoogleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Toast.makeText(WriteActivity.this, "clicked!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        mGoogleMap.setOnMyLocationClickListener(new GoogleMap.OnMyLocationClickListener() {
+            @Override
+            public void onMyLocationClick(@NonNull Location location) {
+                Toast.makeText(WriteActivity.this,
+                        String.format("현재 위치: (%f, %f)", location.getLatitude(), location.getLongitude()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // TODO: 맵 로딩 후 초기에 해야 할 작업 구현
+        markerOptions = new MarkerOptions();
+//        mGeoDataClient = Places.getGeoDataClient(MainActivity.this);
+
+        mGoogleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(final Marker marker) {
+                String placeId = marker.getTag().toString();    // 마커의 setTag() 로 저장한 Place ID 확인
+            }
+        });
+    }
+
+
+    /*구글맵을 멤버변수로 로딩*/
+    private void mapLoad() {
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.writeMap);
+        mapFragment.getMapAsync(this);      // 매배변수 this: MainActivity 가 OnMapReadyCallback 을 구현하므로
+    }
+
+
+    /* 필요 permission 요청 */
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQ_CODE);
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_REQ_CODE) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 퍼미션을 획득하였을 경우 맵 로딩 실행
+                mapLoad();
+            } else {
+                // 퍼미션 미획득 시 액티비티 종료
+                Toast.makeText(this, "앱 실행을 위해 권한 허용이 필요함", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
